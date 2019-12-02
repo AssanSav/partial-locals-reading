3bfdca1acc379d49d8d36ed90d5a45fa4cc0574e
Assane Savadogo <assane.savadogo81@gmail.com>
2019-12-02 11:47:27 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@author = Author.create(name: "Assane", hometown: "Burkina Faso")
+@author.posts << Post.create(title: "My post title", content: "My post content")
